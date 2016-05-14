6db293f25d4ac113f21b5588ea7fe3263604984f
William Muhu <wmuhucraft@gmail.com>
2016-05-14 19:48:19 +0300

    Fixed a bug

--- tellurium.py
+++ tellurium.py
@@ -129,7 +129,7 @@ def parse(cmd):
         tape[selected] = int(tape[selected])
 
     elif cmd == "a":
-        tape[selected] += tape[selected+1]
+        int(tape[selected]) += int(tape[selected+1])
 
     elif cmd == "s":
         tape[selected] -= tape[selected+1]
